9b5c259dc2b12bcd825c67293965710516fb6a46
Soiloquy <yushantang05@gmail.com>
2024-02-03 21:45:29 +0800

    基础样式搭建

--- src/views/bookshelf/index.vue
+++ src/views/bookshelf/index.vue
@@ -1,7 +1,18 @@
 <template>
     <div>
         <Header title="书架" icon="&#xe609;"></Header>
-        书架
+        <div class="header-bookshelf">
+            <button class="all-book">全部图书<span>&#xe8d8;</span></button>
+            <button class="sort">排序<span>&#xe8d8;</span></button>
+        </div>
+        <div class="big-content">
+            <div class="book-content" v-for="item in 11">
+                <div class="time">12天前读过</div>
+                <img src="../../assets/02.jpg" alt="">
+                <div class="intro">天才在左 疯子在右【累计销量破500万册！借疯子的策略，唤醒你未知的灵魂！高圆圆、陈乔恩、李宇春、胡歌、刘昊然盛情推荐！】 (高铭)</div>
+            </div>
+            <div class="button-content"><button>&#xe609;</button></div>
+        </div>
         <Footer></Footer>
     </div>
 </template>
@@ -17,6 +28,84 @@ import Header from '../../components/header-bar-search.vue'
     }
 </script>
 
-<style lang="css" scoped>
+<style lang="less" scoped>
+.header-bookshelf{
+    width: 80%;
+    height: 55px;
+    border-bottom: 1px solid #cacaca;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    .all-book{
+        margin: 20px 0 0 0;
+        display: block;
+        font-size: 18px;
+        span{
+            font-family: "iconfont";
+            font-size: 24px;
+            font-style: normal;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+        }
+    }
+    .sort{
+        margin: 20px 0 0 0;
+        font-size: 18px;
+        span{
+            font-family: "iconfont";
+            font-size: 24px;
+            font-style: normal;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+        }
+    }
+}
+.big-content{
+    display: flex;
+    flex-wrap: wrap;
+    margin-bottom: 30px;
+    .book-content{
+        width: 100px;
+        height: 215px;
+        margin: 20px 0 0 15px;
+        .time{
+            color: #929091;
+            margin:0 0 5px 3px;
+        }
+        img{
+            width: 100%;
+            height: 140px;
+        }
+        .intro{
+            font-size: 14px;
+            margin: 5px 0 0 3px;
+            line-height: 23px;
+            word-break: break-all;
+            overflow: hidden;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+        }
+    }
+    .button-content{
+    width: 100px;
+    height: 215px;
+    margin: 20px 0 0 15px;
+    button{
+        display:inline-block;
+        width: 100%;
+        height: 140px;
+        margin-top: 25px;
+        background-color: #e4e4e4;
+        font-family: "iconfont" !important;
+        font-size: 75px;
+        color: #979797;
+        font-style: normal;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+}
+}
+
 
 </style>
--- src/views/reading/index.vue
+++ src/views/reading/index.vue
@@ -12,6 +12,11 @@
         <span class="title">最近读过</span>
         <div class="book" v-for="item in 5">
             <img src="../../assets/01.jpg" alt="" srcset="">
+            <div class="context">
+                <span class="title">王小波：沉默的大多数（李银河独家授权，并亲自校订全稿。王小波逝世二十周年纪念版！特别收入从未面市的手稿！） (王小波)</span>
+                <span class="time">12分钟前</span>
+                <button class="continue">继续</button>
+            </div>
         </div>
         <div class="tip">注意：确保图书上传到云端，否则无法在其他设备上阅读图书信息</div>
     </div>
@@ -42,7 +47,7 @@ import Header from '../../components/header-bar-base.vue'
         margin: 30px 10px 0 20px;
     }
     .context{
-        width: 160px;
+        width: 150px;
         height: 180px;
         margin: 35px 0 0 10px;
         font-size: 12px;
@@ -94,13 +99,48 @@ import Header from '../../components/header-bar-base.vue'
     .book{
         width: 90%;
         height: 96px;
-        // background-color: #48a6f0;
-        border-bottom: 1px solid #ccc;
+        border-bottom: 1px solid #cfcfcf;
+        display: flex;
         img{
             width: 60px;
             height: 74px;
             margin:11px 0 11px 6px;
         }
+        .context{
+        width: 160px;
+        height: 180px;
+        font-size: 12px;
+        position: relative;
+        .title{
+            width: 170px;
+            font-size: 13px;
+            line-height: 25px;
+            margin: 12px 0 5px 10px;
+            word-break: break-all;
+            overflow: hidden;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+        }
+        .time{
+            display: inline-block;
+            margin-left: 10px;
+            color: #888888;
+        }
+        .continue{
+            display: inline-block;
+            position: absolute;
+            top: 60px;
+            right: -80px;
+            width: 70px;
+            height: 25px;
+            line-height: 25px;
+            font-size: 14px;
+            border-radius: 18px;
+            color: #ffffff;
+            background-color: #48a6f0;
+        }
+    }
     }
 }
 </style>
