cc936d26f6f263b2a64c83a2c379d421e0f12806
Soiloquy <yushantang05@gmail.com>
2024-02-06 15:35:20 +0800

    字体大小设置

---
+++ src/components/Setting.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <transition name="slide">
+            <div class="setting-containter" v-show="SettingFlag">
+                <div class="background-color settings">背景</div>
+                <div class="intensity-control settings">亮度</div>
+                <div class="font-size settings">字体
+                    <button @click="fontSizeDown">A-</button>
+                    <button @click="fontSizeUp">A+</button>
+                </div>
+                <div class="interval settings">间距</div>
+                <!-- <div class="font-family">字体</div> -->
+                <div class="paging settings">翻页</div>
+                <div class="more-setting settings">更多设置</div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script setup>
+import { defineProps,defineEmits } from 'vue';
+
+
+const emit=defineEmits(['fontSizeDown','fontSizeUp'])
+
+const props=defineProps({
+    SettingFlag:{type:Boolean,default:false}
+})
+
+const fontSizeDown=()=>{
+    emit('fontSizeDown')
+}
+const fontSizeUp=()=>{
+    emit('fontSizeUp')
+}
+</script>
+
+<style lang="less" scoped>
+.setting-containter{
+        width: 100%;
+        height: 336px;
+        background-color: #fff;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        z-index: 101;
+        font-size: 18px;
+        box-shadow: 0 -3px 3px rgba(0, 0, 0, .1);
+        &.slide-enter-active,
+        &.slide-leave-active {
+            transition: all 0.1s linear;
+        }
+        &.slide-enter-from,
+        &.slide-leave-to {
+            transform: translateY(100%);
+        }
+        &.slide-enter-to,
+        &.slide-leave-from {
+            transform: translateY(0%);
+        }
+        .settings{
+            width: 85%;
+            height: 56px;
+            margin: 0 auto;
+            line-height: 56px;
+            border-bottom: 1px solid #d4d4d4;
+        }
+        .font-size{
+            display: flex;
+            align-items: center;
+            button{
+                display: block;
+                width: 100px;
+                height: 30px;
+                font-size: 16px;
+                border-radius: 20px;
+                margin-left: 22px;
+                border: 1px solid #1d1d1d;
+                background-color: #fff;
+            }
+        }
+    }
+</style>
--- src/views/ebook/index.vue
+++ src/views/ebook/index.vue
@@ -33,34 +33,55 @@
                 </div>
             </div>
         </transition>
-        <transition name="slide">
-            <div class="setting-containter" v-show="SettingFlag">
-                <div class="background-color settings">背景</div>
-                <div class="intensity-control settings">亮度</div>
-                <div class="font-size settings">字体
-                    <button>A-</button>
-                    <button>A+</button>
-                </div>
-                <div class="interval settings">间距</div>
-                <!-- <div class="font-family">字体</div> -->
-                <div class="paging settings">翻页</div>
-                <div class="more-setting settings">更多设置</div>
-            </div>
-        </transition>
-
+        <Setting :SettingFlag="SettingFlag"
+        @fontSizeDown="fontSizeDown"
+        @fontSizeUp="fontSizeUp"></Setting>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref,reactive } from 'vue';
 import { onMounted } from 'vue';
 import Epub from 'epubjs'
+import Setting from '../../components/Setting.vue'
 
 let rendition=''
 const DOWNLOAD_URL='/01.epub'
 let MenuShowFlag=ref(false)
 let SettingFlag=ref(true)
+const defaultFontSize=18
+let fontSizeIndex=3
+const FontSize=reactive([
+    {fontSize:12},
+    {fontSize:14},
+    {fontSize:16},
+    {fontSize:18},
+    {fontSize:20},
+    {fontSize:22},
+    {fontSize:24},
+])
+let themes
 
+const fontSizeDown=()=>{
+    if (fontSizeIndex<=0) {
+        console.log('最小了');
+        return
+    }else{
+        fontSizeIndex--
+        console.log(FontSize[fontSizeIndex].fontSize,fontSizeIndex);
+        themes.fontSize(FontSize[fontSizeIndex].fontSize+'px')
+    }
+}
+const fontSizeUp=()=>{
+    if (fontSizeIndex>=6) {
+        console.log('最大了');
+        return
+    }else{
+        fontSizeIndex++
+        console.log(FontSize[fontSizeIndex].fontSize,fontSizeIndex);
+        themes.fontSize(FontSize[fontSizeIndex].fontSize+'px')
+    }
+}
 const showEpub=()=>{
     let book=Epub(DOWNLOAD_URL)
 
@@ -69,6 +90,8 @@ const showEpub=()=>{
         height:window.innerHeight
     })
     rendition.display()
+    themes=rendition.themes
+    themes.fontSize(defaultFontSize+'px')
 }
 const prevPage=()=>{
     if (SettingFlag.value!=true) {
@@ -99,6 +122,7 @@ const settingsChange=()=>{
         MenuShowFlag.value=false
     }
 }
+
 onMounted(()=>{
     showEpub()
 })
@@ -200,51 +224,6 @@ onMounted(()=>{
         .word{
             font-size: 18px;
             color: #646464;
-        }
-    }
-    .setting-containter{
-        width: 100%;
-        height: 336px;
-        // background-color: aqua;
-        background-color: #fff;
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        z-index: 101;
-        font-size: 18px;
-        box-shadow: 0 -3px 3px rgba(0, 0, 0, .1);
-        &.slide-enter-active,
-        &.slide-leave-active {
-            transition: all 0.1s linear;
-        }
-        &.slide-enter-from,
-        &.slide-leave-to {
-            transform: translateY(100%);
-        }
-        &.slide-enter-to,
-        &.slide-leave-from {
-            transform: translateY(0%);
-        }
-        .settings{
-            width: 85%;
-            height: 56px;
-            margin: 0 auto;
-            line-height: 56px;
-            border-bottom: 1px solid #d4d4d4;
-        }
-        .font-size{
-            display: flex;
-            align-items: center;
-            button{
-                display: block;
-                width: 100px;
-                height: 30px;
-                font-size: 16px;
-                border-radius: 20px;
-                margin-left: 22px;
-                border: 1px solid #1d1d1d;
-                background-color: #fff;
-            }
         }
     }
 }
