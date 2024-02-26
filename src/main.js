c07faef7dcf02a13f2e658b16c50cad9922d22c6
Soiloquy <yushantang05@gmail.com>
2024-02-26 16:23:16 +0800

    添加Vuex仓库

--- src/main.js
+++ src/main.js
@@ -6,8 +6,10 @@ import './assets/fonts/iconfont.css'
 import 'amfe-flexible'
 import router from './router'
 import { IonicVue } from '@ionic/vue';
+import store from './store'
 
 const app=createApp(App)
 app.use(IonicVue)
 app.use(router)
+app.use(store)
 app.mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import { createStore } from "vuex";
+
+export default createStore({
+    state:{
+        // 存储当前页面的cfi
+        nowPageEpubCFI:'',
+        // 存储当前页面的percentage
+        nowPagePercentage:0
+    },
+    mutations:{
+        getEpubCFIandPercentage(state,[cfi,percentage]){
+            state.nowPageEpubCFI=cfi
+            state.nowPagePercentage=percentage
+            console.log(state.nowPageEpubCFI,percentage);
+        }
+    },
+    actions:{
+
+    },
+    getters:{
+
+    }
+})
